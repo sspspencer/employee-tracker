d1d6eeb3e4cd7a005a8702a0f899b6125d19514d
Spencer Pichette <ssptuningperformance@gmail.com>
2022-08-21 12:48:28 -0400

    database and sql set up

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (name) VALUES
+    ('Engineering'),
+    ('Finance'),
+    ('Legal'),
+    ('Sales');
+
+INSERT INTO role (title, salary, department_id) VALUES
+    ('Sales Lead', 100000, 4),
+    ('Salesperson', 80000, 4),
+    ('Lead Engineer', 150000, 1),
+    ('Software Engineer', 120000, 1),
+    ('Account Manager', 160000, 2),
+    ('Accountant', 125000, 2),
+    ('Legal Team Lead', 250000, 3),
+    ('Lawyer', 190000, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager) VALUES
+    ('John', 'Doe', 1, NULL),
+    ('Mike', 'Chan', 2, 'John Doe'),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 'Ashley Rodriguez'),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 'Kunal Singh'),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 'Sarah Lourd');
